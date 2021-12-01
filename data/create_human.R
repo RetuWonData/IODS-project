c3259ddc32ccb48ddee42e4a4b182c8b60613195
Retu Haikonen <retuh@uef.fi>
2021-12-01 15:54:16 +0200

    Week 5 data part.

--- data/create_human.R
+++ data/create_human.R
@@ -1,6 +1,8 @@
 # Rhaikonen
 # 28.11.2021
 # Wrangling the human data
+library(dplyr)
+library(pillar)
 
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
@@ -33,17 +35,88 @@ gii <- mutate(gii,
               edu2_ratio = edu2F/edu2M,
               lab_ratio = labF/labM)
 
-#joining the two datasets (1p)
+#joining two data sets
 human <- inner_join(hd,gii,by=c("Country"))
 dim(human) # 195 X 19
 summary(human)
 
-#saving the data to the 'data' folder
+# Writing the data
 
 write.csv(human, "data/human.csv", row.names = F)
 
 # Optional importation (to uncomment select, and press Ctrl + Shift + C)
-human2 <- read.csv("data/human.csv",header = T)
-dim(human2)
-glimpse(human2)
-rm(human2)
+# human2 <- read.csv("data/human.csv",header = T)
+# dim(human2)
+# glimpse(human2)
+# rm(human2)
+
+###############################################################################
+###############################################################################
+# 1.12.2021
+# Still RHaikonen on the other side of the screen
+###############################################################################
+###############################################################################
+
+human <- read.csv("data/human.csv",header = T)
+dim(human) # 195 observations in 19 variables
+library(pillar)
+glimpse(human)
+
+# Original data from: http://hdr.undp.org/en/content/human-development-index-hdi
+# Data have following variables
+# "Country" = Country name
+ 
+# # Health and knowledge
+# "HDI_Rank" = Human development index rank
+# "HDI" = Human development index
+# "Exp_Life" = Life expectancy at birth
+# "Exp_Educ" = Expected years of schooling
+# "Mean_Educ" = Mean years of education
+# "GNI" = Gross National Income per capita
+# "GNI_HDI" = GNI per capita rank - HDI rank
+# "GII_Rank" = Gender inequality index rank
+# "GII" = Gender inequality index
+# "Mort_ratio" = Maternal mortality ratio
+# "ABR" = Adolescent birth rate
+
+# # Empowerment
+# "PRP" = Percetange of female representatives in parliament
+# "edu2F" = Proportion of females with at least secondary education
+# "edu2M" = Proportion of males with at least secondary education
+# "labF" = Proportion of females in the labour force
+# "labM" " Proportion of males in the labour force
+# "edu2_ratio" = Edu2.F / Edu2.M
+# "lab_ratio" = Labo2.F / Labo2.M
+
+# GNI to numeric
+
+library(stringr)
+str(human$GNI)
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+# Excluding data that is not needed
+keep <- c("Country", "edu2_ratio", "lab_ratio", "Exp_Life", "Exp_Educ", "GNI", "Mort_ratio", "ABR", "PRP")
+human <- select(human, one_of(keep))
+
+# Excluding not complete cases i.e. missing values
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human_ <- filter(human, complete.cases(human))
+
+# Still excluding, this time areas
+tail(human_, 12) # 7 last are not countries but areas instead
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
+dim(human_) # 155 countries left
+
+# Setting the row names
+rownames(human_) <- human_$Country
+human <- human_[, -1]
+dim(human) # now 155 observations (countries) in 8 variables
+
+write.csv(human, "data/human.csv", row.names = T)
+# Again, optional importation (to uncomment select, and press Ctrl + Shift + C)
+# human2 <- read.csv("data/human.csv",header = T, row.names = 1)
+# dim(human2)
+# glimpse(human2)
+# rm(human2)
